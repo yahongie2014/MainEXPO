0d6b2ca82ed60a3158bc9e6f73fe9170c92f94ef
Ahmed Saeed <saeed@details.net.sa>
2022-02-19 12:48:08 +0200

    ad type script

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
-const createExpoWebpackConfigAsync = require("@expo/webpack-config");
-
 const path = require("path");
-module.exports = async function (env, argv, loaders) {
-  const config = await createExpoWebpackConfigAsync(env, argv, loaders);
-  loaders = [
-    {
-      test: /\.(png|woff|woff2|eot|ttf|svg|jpg|jpeg|otf)$/,
-      loader: "url-loader?limit=100000",
-    },
-  ];
+const createExpoWebpackConfigAsync = require("@expo/webpack-config");
+const node_modules = path.resolve(__dirname, "node_modules");
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+
+  config.module.rules.push({
+    test: /\.(png|woff|woff2|eot|ttf|svg|jpg|jpeg|otf)$/,
+    include: /(packages|example)\/.+/,
+    exclude: /node_modules/,
+    use: "babel-loader",
+  });
+
   // If you want to add a new alias to the config.
+
   config.resolve.extensions["extensions"] = [".js", ".jsx", ".web.js"];
-  config.resolve.alias = {
+  Object.assign(config.resolve.alias, {
     ...config.resolve.alias,
-    modulesDirectories: path.resolve(__dirname, "node_modules"),
+    react: path.resolve(node_modules, "react"),
+    "react-native": path.resolve(node_modules, "react-native-web"),
+    "react-native-web": path.resolve(node_modules, "react-native-web"),
+    "@expo/vector-icons": path.resolve(node_modules, "@expo/vector-icons"),
+    "react-native-reanimated": path.resolve(
+      node_modules,
+      "react-native-reanimated"
+    ),
+    modulesDirectories: path.resolve(node_modules),
     "@components": path.resolve(__dirname, "src/Components/"),
     "@containers": path.resolve(__dirname, "src/Containers/"),
     "@config": path.resolve(__dirname, "src/Config/"),
@@ -25,12 +37,13 @@ module.exports = async function (env, argv, loaders) {
     "@images": path.resolve(__dirname, "assets/images/*"),
     "@assets": path.resolve(__dirname, "assets/images/*"),
     "@fonts": path.resolve(__dirname, "assets/fonts/*"),
-  };
+  });
   if (config.mode === "development") {
     config.devServer.compress = false;
   }
   if (config.mode === "production") {
     config.optimization.minimize = false;
   }
+
   return config;
 };
