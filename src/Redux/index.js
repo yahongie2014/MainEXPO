251e8e8f22f3fe6650162ada9239611336ca428b
Ahmed Saeed <saeed@details.net.sa>
2022-02-19 10:42:42 +0200

    Fix Bable Config  Root

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
-import app from "@redux/reducers";
+import app from "@reducers";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const createExpoWebpackConfigAsync = require("@expo/webpack-config");
-
+const path = require("path");
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   // If you want to add a new alias to the config.
-  config.resolve.alias["moduleA"] = "moduleB";
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    "@components": path.resolve(__dirname, "src/Components/"),
+    "@containers": path.resolve(__dirname, "src/Containers/"),
+    "@config": path.resolve(__dirname, "src/Config/"),
+    "@redux": path.resolve(__dirname, "src/Redux/"),
+    "@reducers": path.resolve(__dirname, "src/Redux/reducers/"),
+    "@actions": path.resolve(__dirname, "src/Redux/actions/"),
+    "@navigation": path.resolve(__dirname, "src/Navigation/"),
+    "@lang": path.resolve(__dirname, "src/lang/"),
+    "@images": path.resolve(__dirname, "assets/images/*"),
+    "@assets": path.resolve(__dirname, "assets/images/*"),
+    "@fonts": path.resolve(__dirname, "assets/fonts/*"),
+  };
   if (config.mode === "development") {
     config.devServer.compress = false;
   }
